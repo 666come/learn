7c98eda18abcf0e2cbcfac523e58fde32db1f4eb
666come <102793218+666come@users.noreply.github.com>
2022-05-04 15:32:25 +0800

    Add files via upload

---
+++ 20220504/20220504/牛客网练习题.cpp
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//输出结果
+//int main() {
+//    int m = 12, n = 34;
+//    printf("%d%d", m++, ++n);
+//    printf("%d%d\n", n++, ++m);
+//}
+
+//
+//#include<string.h>
+//int  main(void) {
+//    int x;
+//    int len;
+//    len = strlen("I See, Sea in C");
+//    //空格也算长度
+//    printf("%d\n", len);
+//  
+//}
+
+
+//fork函数是C++的函数
+//int main() {
+//	printf("Hello");
+//	fork();
+//	printf("Hello");
+//}
+
+
+//运算符优先级
+//int main()
+//{
+//	int x = 10;
+//
+//	x += x -= x - x;
+//	printf("%d\n", x);
+//	return 0;
+//}
+
+#include<iostream>
+#include<cstdio>
+using namespace std;
+int main() {
+	int m = 0123, n = 123;
+	printf("%o %o\n", m, n);
+	return 0;
+}

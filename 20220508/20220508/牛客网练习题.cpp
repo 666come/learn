5d7de88b0ee45102c2e68f2185fbbff9f5e8d2d1
666come <102793218+666come@users.noreply.github.com>
2022-05-08 16:36:19 +0800

    Add files via upload

---
+++ 20220508/20220508/牛客网练习题.cpp
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+
+//验证是否正确？
+//int main()
+//{
+//	int c;
+//	while (c = getchar());
+//
+//	return 0;
+//}
+
+//#include<string.h>
+//#include<iostream>
+//using namespace std;
+//char* f(char* str, char ch) {
+//    char* it1 = str;
+//    char* it2 = str;
+//    while (*it2 != '\0') {
+//        while (*it2 == ch) 
+//        { it2++; }
+//        *it1++ = *it2++;
+//    }
+//    return str;
+//}
+//int main(int argc, char* argv[]) {
+//    char* a = new char[10];
+//    strcpy(a, "abcdcccd");
+//    cout << f(a, 'c');
+//}
+
+//int main()
+//{
+//	int A[2][3] = { 1, 2, 3, 4, 5, 6 };  
+//	
+//	return 0;
+//}
+
+
+//输出结果
+//int fun3(int x)
+//{
+//    static int a = 2;
+//    a += x;
+//    return(a);
+//}
+//void main()
+//{
+//    int k = 2, m = 1, n;
+//    n = fun3(k);
+//    n += fun3(m);
+//    printf("%d\n", n);
+//}
+
+

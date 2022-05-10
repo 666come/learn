7cad0f47bab6274f07badd5138456b719e094af1
666come <102793218+666come@users.noreply.github.com>
2022-05-10 15:03:42 +0800

    Add files via upload

---
+++ 20220510/20220510/牛客网练习题.cpp
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//int main()
+//{
+//	int j, k;
+//	for (j = 0, k = -1; k = 1; j++, k++)//判断语句恒为真
+//		printf("****\n");
+//	return 0;
+//}
+
+//输出结果
+//int Xfun(int* a, int n) {
+//    int x = *a;
+//    for (int* pa = a + 1; pa < a + n; pa++)
+//        if (*pa > x) x = *pa;
+//    return x;
+//}
+//int main()
+//{
+//    int x[5] = { 23,46,78,55,16 };
+//    int res = Xfun(x, 5);
+//    printf("%d\n", res);
+//    return 0;
+//}
+
+//结果
+//int main()
+//{
+//    int a[5] = { 1, 2, 3, 4, 5 };
+//    int* ptr = (int*)(&a + 1);
+//    printf("%d, %d", *(a + 1), *(ptr - 1));
+//    return 0;
+//}
+
+int new(int c)
+{
+
+}
+struct test
+{
+	new(5);
+};
+int main()
+{
+	struct test;
+	return 0;
+}
